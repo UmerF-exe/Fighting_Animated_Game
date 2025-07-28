3469e6705fdef057f66749d137fc89927ffdd0e0
Umer Farooque <mohranvi4484@gmail.com>
2025-07-28 22:40:53 +0500

    completed

---
+++ app.js
@@ -0,0 +1,113 @@
+var leo = document.getElementById("leo");
+var leoPosition = 10;
+var leoForward = false;
+var leoBackward = false;
+var leoPower = false;
+var chacha = document.getElementById("chacha");
+var chachaPosition = 10;
+var chachaForward = false;
+var chachaBackward = false;
+var chachaPower = false;
+
+function moveChacha(type){
+    if(type === 'forward'){
+        chachaBackward = false;
+        chachaPosition += 5;
+        chacha.style.right = chachaPosition + "px";
+        chacha.style.width = 600 + "px";
+        chacha.style.height = 600 + "px";
+        if(!leoForward){
+        chacha.src = "Images/chacha-forward.gif";
+        }
+        chachaForward = true;
+        // setTimeout(function (){
+        //     leo.src = "Images/leo.gif";
+        //     leoForward = false;
+        // }, 2000);
+    }
+    if(type === 'backward'){
+        chachaForward = false;
+        chachaPosition -= 5;
+        chacha.style.right = chachaPosition + "px";
+        chacha.style.width = 500 + "px";
+        chacha.style.height = 500 + "px";
+        if(!chachaBackward){
+            chacha.src = "Images/chacha-backward.gif";
+        }
+        chachaBackward = true;
+        // setTimeout(function (){
+        //     leo.src = "Images/leo.gif";
+        // }, 2000);
+    }
+    // if(type === " "){
+    //     if(!chachaPower){
+    //         chacha.src = "Images/leo-power.gif";
+    //     }
+    //     chachaPower = true;
+    // }
+}
+function moveLeo(type){
+    if(type === 'forward'){
+        leoBackward = false;
+        leoPosition += 5;
+        leo.style.left = leoPosition + "px";
+        if(!leoForward){
+        leo.src = "Images/leo-forward.gif";
+        }
+        leoForward = true;
+        // setTimeout(function (){
+        //     leo.src = "Images/leo.gif";
+        //     leoForward = false;
+        // }, 2000);
+    }
+    if(type === 'backward'){
+        leoForward = false;
+        leoPosition -= 5;
+        leo.style.left = leoPosition + "px";
+        if(!leoBackward){
+            leo.src = "Images/leo-backward.gif";
+        }
+        leoBackward = true;
+        // setTimeout(function (){
+        //     leo.src = "Images/leo.gif";
+        // }, 2000);
+    }
+    if(type === " "){
+        if(!leoPower){
+            leo.src = "Images/leo-power.gif";
+        }
+        leoPower = true;
+    }
+}
+function action(event){
+    if(event.key === 'd'){
+        moveLeo('forward');
+    }
+    else if(event.key === 'a'){  
+        moveLeo('backward');
+    }
+    else if(event.key === " "){
+        moveLeo(" ");
+    }
+    else if(event.key === 'ArrowLeft'){  
+        moveChacha('forward');
+    }
+    else if(event.key === 'ArrowRight'){  
+        moveChacha('backward');
+    }
+}
+window.addEventListener('keydown', action);
+
+function resetAction(){
+    leo.src = "Images/leo.gif";
+    chacha.src = "Images/chacha.gif";
+    leoForward = false;
+    leoBackward = false;
+    leoPower = false;
+    chachaForward = false;
+    chachaBackward = false;
+    chachaPower = false;
+    chacha.style.width = 300 + "px";
+    chacha.style.height = 300 + "px";
+}
+window.addEventListener('keyup', resetAction);
